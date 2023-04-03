8ccda5017eaf9d3b81a9e7e0ecfaa9854d5a0a03
Vy Nguyen <vy.nguyen@logixtek.com>
2023-04-03 10:39:47 +0700

    Add code chat feature

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  extends: [
+    "@remix-run/eslint-config",
+    "@remix-run/eslint-config/node",
+    "prettier",
+  ],
+  plugins: ["prettier", "simple-import-sort"],
+  ignorePatterns: ["**/vendor/*.js", "build/*", "*.test.js"],
+  rules: {
+    "prettier/prettier": ["error"],
+    "simple-import-sort/imports": [
+      "error",
+      {
+        groups: [
+          // Packages `react` related packages come first.
+          ["^react", "^@?\\w"],
+          // Internal packages.
+          ["^(@|src)(/.*|$)"],
+          // Side effect imports.
+          ["^\\u0000"],
+          // Parent imports. Put `..` last.
+          ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
+          // Other relative imports. Put same-folder imports and `.` last.
+          ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
+          // Style imports.
+          ["^.+\\.?(css)$"],
+        ],
+      },
+    ],
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        js: "never",
+        jsx: "never",
+        ts: "never",
+        tsx: "never",
+      },
+    ],
+  },
+};
---
+++ app/theme/index.js
@@ -0,0 +1,9 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export const theme = extendTheme({
+  fonts: {
+    body: "Inter system-ui, sans-serif",
+    heading: "Inter Georgia, serif",
+    mono: "Inter Menlo, monospace",
+  },
+});
