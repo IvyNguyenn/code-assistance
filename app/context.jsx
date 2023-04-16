33fa85a8d77d6018fd8204289f32ade038fd43e8
HoangVy <hoangvynguyen42@gmail.com>
2023-04-16 15:24:12 +0700

    Adjust init source

--- app/context.jsx
+++
@@ -1,5 +0,0 @@
-import React, { createContext } from "react";
-
-export const ServerStyleContext = createContext(null);
-
-export const ClientStyleContext = createContext(null);
--- app/theme/index.js
+++
@@ -1,9 +0,0 @@
-import { extendTheme } from "@chakra-ui/react";
-
-export const theme = extendTheme({
-  fonts: {
-    body: "Inter system-ui, sans-serif",
-    heading: "Inter Georgia, serif",
-    mono: "Inter Menlo, monospace",
-  },
-});
